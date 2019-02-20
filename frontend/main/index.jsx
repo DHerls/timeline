0c1d876f3cde070defad549f24dde017eb9caec5
Dan Herlihy <djherls@gmail.com>
2019-02-19 21:24:22 -0500

    Add migrations

--- frontend/main/index.jsx
+++ frontend/main/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup 2';
+class TestForm extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {value: 'Synced input'};
+	    this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+
+	render() {
+		return (
+			<label>
+			Things:
+			<input type="text" value={this.state.value} onChange={this.handleChange} />
+			<p>{this.state.value}</p>
+			</label>
+		)
+	}
+}
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <TestForm />,
   document.getElementById('app')
 );
 
